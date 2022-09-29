c6e1b0e765793d78fa043b1d5dbfa091df3703e9
NikolaiBelski <95039914+NikolaiBelski@users.noreply.github.com>
2022-09-29 21:37:15 +0300

    two

--- src/components/Success.js
+++ src/components/Success.js
@@ -4,7 +4,7 @@ export const Success = ({ count }) => {
   return (
     <div class="success-block">
       <img src="/assets/success.svg" alt="Success" />
-      <h3>Успешно!</h3>
+      <h3>Успешно, всего доброго</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
       <button className="send-invite-btn" onClick = {()=>window.location.reload()}>Назад</button>
     </div>
